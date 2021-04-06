995ce8604373665c333b44d574b8e81fe0cbc246
Tom Duex <thomasduex@gmail.com>
2021-04-06 16:57:20 -0500

    Additional Strategies

--- src/tempData/allStrats.js
+++ src/tempData/allStrats.js
@@ -2,14 +2,14 @@ export default [
     {
         "name": "Bait and Switch",
         "description": "Have two people buy Marshalls and three people buy shotguns. Work in teams, use the Marshal players to bait enemies into charging, where your shotgun counterpart is waiting around the corner. See how many times the enemy team falls for it.",
-        "offense": true,
+        "offense": false,
         "defense": true,
         "specificMaps": []
     },
     {
         "name": "Warm Welcome",
         "description": "Buy 5 Odins and stack up on one door. This one isn't complicated.",
-        "offense": true,
+        "offense": false,
         "defense": true,
         "specificMaps": []
     },
@@ -57,9 +57,9 @@ export default [
     },
     {
         "name": "Violence is not the answer",
-        "description": "Any time you encounter resistance on the way to any bomb site, you must change course and try another way. Only after you plant the spike are you allowed to use violence, when it is absolutely necessary.",
+        "description": "Any time you encounter resistance on the way to any spike site, you must change course and try another way. Only after you plant the spike are you allowed to use violence, when it is absolutely necessary.",
         "offense": true,
-        "defense": true,
+        "defense": false,
         "specificMaps": []
     },
     {
@@ -73,12 +73,131 @@ export default [
         "name": "Sunblock",
         "description": "You must use the maximum amount of smoke effects possible to blanket one of the sites as you approach. Bonus points if you can plant the spike without seeing the sky on your way there.",
         "offense": true,
-        "defense": true,
+        "defense": false,
         "specificMaps": []
     },
     {
         "name": "I'm not an ambi-turner",
         "description": "entire team chooses one 'wasd' key, for the round, your team may not use that key.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Versatile Arsenal",
+        "description": "Your team must buy/use one primary gun from each of the five categories. (SMGs, Shotguns, Rifles, Sniper, Heavy)",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Wholesome Moment",
+        "description": "Compliment your teammates on every good thing they do.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Spread The Love",
+        "description": "Compliment the enemy team on every good thing they do.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Captain Picard",
+        "description": "The bottom player on the leaderboard is the captain. Whenever you want to use a teleporter, you must first ask the captain for permission. You cannot use the teleporter until permission is granted.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": ["Bind"]
+    },
+    {
+        "name": "Broadcast Booth",
+        "description": "When you die, join the broadcast booth and provide some exciting commentary for the round.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Controlled Effort",
+        "description": "Everyone buys the Stinger or Bulldog. You can only shoot using the ADS burst fire.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Zipline",
+        "description": "The entire team must use the zipline into A to get into the spike site. You may not peek the main entry, and you must ride the zipline until you're at least above the site.",
+        "offense": true,
+        "defense": false,
+        "specificMaps": ["Icebox"]
+    },
+    {
+        "name": "I have the high ground",
+        "description": "Gain the tactical advantage by only fighting from higher ground whenever possible, or even ground if absolutely necessary. Never fight from lower ground.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Bounty Hunters",
+        "description": "You must kill the enemy team in order of highest to lowest on the leaderboard.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "They wouldn't do it a third time",
+        "description": "Before rounding a corner into the enemy, be sure to flash the corner three times for mind games.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Scenic Route",
+        "description": "Follow this route: Mid > Zip Zap > A Heaven > Defender Spawn > B Heaven > B Site",
+        "offense": true,
+        "defense": false,
+        "specificMaps": ["Split"]
+    },
+    {
+        "name": "Tour Guide",
+        "description": "The third player on the leaderboard is the team tour guide. Lead the team around the map and detail what they're seeing. If you encounter any enemies, treat the encounter as an unexpected tour experience.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Piracy",
+        "description": "Buy the shorty or frenzy and play those corners. Try to steal and use as many of the enemy's weapons as possible. Bonus points if you talk like a pirate the entire round.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Pressed For Time",
+        "description": "Wait in spawn until nearly the last second, then rush a spike site with just enough time to plant.",
+        "offense": true,
+        "defense": false,
+        "specificMaps": []
+    },
+    {
+        "name": "Quickest Reactions in the West",
+        "description": "Set your mouse sensitivity to the highest level. React quicker than you've ever reacted before.",
+        "offense": true,
+        "defense": true,
+        "specificMaps": []
+    },
+    {
+        "name": "Who Needs Spikes?",
+        "description": "Your team may not plant the spike.",
+        "offense": true,
+        "defense": false,
+        "specificMaps": []
+    },
+    {
+        "name": "Antiquated Battle Tactics",
+        "description": "The whole team buys Marshalls and the top player on the leaderboard is the squad commander. Move as a group, and only fire shots when the commander says 'Fire!'.",
         "offense": true,
         "defense": true,
         "specificMaps": []
